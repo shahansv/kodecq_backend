d3829b2714bb65f178ff2f1af970a8d0abd1655a
shahansv <shahanvsaleem@gmail.com>
2025-12-20 18:46:50 +0530

    Workspace added

--- routes.js
+++ routes.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const authController = require("./controllers/authController");
-const {
-  createWorkspace,
-  checkWorkspaceExist,
-} = require("./controllers/workSpaceController");
+const workSpaceController = require("./controllers/workSpaceController");
 
 const routes = new express.Router();
 
 routes.post("/register", authController.registerUser);
 routes.post("/login", authController.loginUser);
 routes.post("/googleLogin", authController.googleLogin);
-routes.post("/createWorkspace", createWorkspace);
-routes.get("/checkWorkspaceExist/:code", checkWorkspaceExist);
+routes.post("/createWorkspace", workSpaceController.createWorkspace);
+routes.get(
+  "/checkWorkspaceExist/:code",
+  workSpaceController.checkWorkspaceExist
+);
 
 module.exports = routes;
