9148000ed19833d0f65751520ca8007823d19aff
shahansv <shahanvsaleem@gmail.com>
2025-12-20 20:56:00 +0530

    authentication completed

---
+++ middlewares/jwtMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const jwtMiddleware = (req, res, next) => {
+  let token = req.headers.authorization.split(" ")[1];
+  try {
+    if (token) {
+      let decodedData = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      if (decodedData) {
+        req.user = decodedData.email;
+        next();
+      } else {
+        res.status(401).json({ message: "Invalied Token , Please login" });
+      }
+    } else {
+      res.status(401).json({ message: "Token is requied , Please login" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message:
+        "Somthing went wrong while validating token,please try login again",
+    });
+  }
+};
+
+module.exports = jwtMiddleware;
--- routes.js
+++ routes.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const authController = require("./controllers/authController");
 const workspaceController = require("./controllers/workspaceController");
+const jwtMiddleware = require("./middlewares/jwtMiddleware");
 
 const routes = new express.Router();
 
 routes.post("/register", authController.registerUser);
 routes.post("/login", authController.loginUser);
 routes.post("/googleLogin", authController.googleLogin);
-routes.post("/createWorkspace", workspaceController.createWorkspace);
-routes.get(
-  "/checkWorkspaceExist/:code",
-  workspaceController.checkWorkspaceExist
-);
+routes.post("/createWorkspace", jwtMiddleware, workspaceController.createWorkspace);
+routes.get("/checkWorkspaceExist/:code", jwtMiddleware, workspaceController.checkWorkspaceExist);
 
 module.exports = routes;
