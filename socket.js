535f31841c3115162507e1b121be5fee6a4a68a5
shahansv <shahanvsaleem@gmail.com>
2025-12-30 00:31:01 +0530

    code sync added

--- socket.js
+++ socket.js
@@ -11,6 +11,14 @@ module.exports = (server) => {
   io.on("connection", (socket) => {
     console.log("Socket connected:", socket.id);
 
+    socket.on("joinWorkspace", (workspaceID) => {
+      socket.join(workspaceID);
+      console.log(`Socket ${socket.id} joined workspace ${workspaceID}`);
+    });
+
+    socket.on("codeChange", ({ workspaceID, code }) => {
+      socket.to(workspaceID).emit("code-update", code);
+    });
 
     socket.on("disconnect", () => {
       console.log("Socket disconnected:", socket.id);
